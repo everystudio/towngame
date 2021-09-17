ef9c9d4c5ac7e278f01fcf221ab0f8b37b389afd
watanabe@tiffa <watanabe.everystudio@gmail.com>
2021-09-17 16:34:27 +0900

    ゲーム画面のタイルを追加

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -5,13 +5,27 @@ using DG.Tweening;
 
 public class PlayerController : MonoBehaviour
 {
-	public Vector2Int m_PlayerPos;
+	public Vector2Int m_PlayerGrid;
 
 	public void Move(ARROW_DIR _dir)
 	{
-		m_PlayerPos += Defines.arrow_vecs[(int)_dir];
+		m_PlayerGrid += Defines.arrow_vecs[(int)_dir];
+		Vector2 targetPos = GetPosition(m_PlayerGrid);
 
-		transform.DOMove(new Vector3((float)m_PlayerPos.x, (float)m_PlayerPos.y, 0f), 0.25f);
+		transform.DOMove(
+			new Vector3(
+				(float)targetPos.x,
+				(float)targetPos.y,
+				0f), 0.25f);
+	}
+
+	private Vector2 GetPosition( Vector2Int _grid)
+	{
+		Vector2 ret = new Vector2(
+			0.5f + _grid.x,
+			0.5f + _grid.y
+			);
+		return ret;
 	}
 
 }
